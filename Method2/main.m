44c4c6b657144041d86e3b0e4af4472dc18eb3bd
Olivia Zhao <mavis.zfy@gmail.com>
2019-05-05 16:52:38 -0500

    organized

---
+++ Method2/main.m
@@ -0,0 +1,37 @@
+run('vlfeat/toolbox/vl_setup.m');
+clear;
+
+% Parameters
+num_frames = 1000;
+frame_dir = '360/frames/';
+out_dir = '360/frames_opt/';
+im_size = [360 640];
+crop_ratio = 0.8;
+
+% Read Images
+im_array = readImages(frame_dir, num_frames);
+
+% Extract SIFT features
+[features, descriptors] = extractSIFT(im_array);
+
+% Get original camera path
+t_transforms = getTransforms(im_array, features, descriptors);
+
+%% Save and load variables to reduce re-run times
+% save('variables.mat', 'im_array', 't_transforms');
+% load('variables.mat');
+
+% Get optimized camera path
+n_transforms = optimizeTransforms(t_transforms, im_size);
+
+% Plot camera paths
+plotPath(t_transforms, n_transforms);
+
+% Apply new camera path and crop
+n_im_array = applyOptimizedTransforms(im_array, n_transforms);
+
+% Save frames
+for k=1:num_frames
+    file_name = fullfile(out_dir, sprintf('%4d.jpg',k));
+    imwrite(n_im_array{k}, file_name);
+end
--- SIFT/merge.sh
+++
@@ -1,9 +0,0 @@
-ffmpeg \
-  -i orig.mp4 \
-  -i stable.mp4 \
-  -filter_complex '[0:v]pad=iw*2:ih[int];[int][1:v]overlay=W/2:0[vid]' \
-  -map [vid] \
-  -c:v libx264 \
-  -crf 23 \
-  -preset veryfast \
-  merged.mp4
